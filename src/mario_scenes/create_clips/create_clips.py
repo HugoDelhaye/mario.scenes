5897ad1a42f64cbf45e1123c363093fb7a562395
Yann Harel <yharel109@gmail.com>
2025-02-27 21:11:20 -0500

    fixed

--- src/mario_scenes/create_clips/create_clips.py
+++ src/mario_scenes/create_clips/create_clips.py
@@ -15,12 +15,8 @@ from tqdm import tqdm
 from tqdm_joblib import tqdm_joblib
 import traceback
 from mario_scenes.load_data import load_scenes_info
-<<<<<<< HEAD
 from mario_replays.utils import replay_bk2, get_variables_from_replay, make_mp4, make_gif, make_webp
 from mario_replays.load_data import collect_bk2_files
-=======
-from mario_replays.utils import replay_bk2, get_variables_from_replay, make_mp4, make_gif, make_webp, collect_bk2_files, create_sidecar_dict
->>>>>>> refs/remotes/origin/main
 
 def prune_variables(variables, start_idx, end_idx):
     """
